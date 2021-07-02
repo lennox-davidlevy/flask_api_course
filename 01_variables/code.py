16b919ceb89511749afbbea9d47270aceab99406
David Levy <lennox.davidlevy@gmail.com>
2021-07-02 19:28:48 -0400

    string formatting

---
+++ 01_variables/code.py
@@ -0,0 +1,20 @@
+x = 1500
+price = 9.99
+discount = 0.2
+
+result = price * (1 - discount)
+
+print('----------\n')
+print(result)
+result = 1
+name = "David"
+print('----------\n')
+print(name)
+print(name * 2)
+
+a = 25
+b = a
+
+print('----------\n')
+print(a)
+print(b)
